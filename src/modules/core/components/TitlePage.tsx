2a477817cacc7b3631be603bd1baa854e808de4d
Kevin <326.osito@gmail.com>
2025-01-28 19:45:58 -0400

    change: header css

--- src/modules/core/components/TitlePage.tsx
+++ src/modules/core/components/TitlePage.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export const TitlePage = ({ title, description }: Props) => {
   return (
-    <header className='categories__header'>
+    <header className='ui__header'>
       <div className='container'>
         <h1 className='text-3xl'>{title}</h1>
         <p>{description}</p>
